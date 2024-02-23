a254ce9e65a2a8f3ae083d549e06cc915835697f
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-23 16:00:49 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import pandas as pd
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/v1/<word>")
+def meaning(word):
+    df = pd.read_csv("dictionary.csv")
+    meaning = df.loc[df['word']=="facing"]['definition'].squeeze()
+    return {"Meaning": meaning,
+            "word": word}
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
